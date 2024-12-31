b74e5acd966dba47eb8c71203f357c31c2aed6ae
joshrossman <josh.rossman@gmail.com>
2024-12-30 22:30:16 -0500

    new

--- part3.js
+++ part3.js
@@ -10,6 +10,24 @@ function changeFontColor(){
     document.getElementById('box').style.color=myFontColor.value;   
 }
 
+function changeUpper(){
+    if(document.getElementById('myUpper').checked)
+        document.getElementById('box').style.textTransform="uppercase";     
+    else
+        document.getElementById('box').style.textTransform="lowercase";
+         
+}
+function changeFont(){
+    document.getElementById('box').style.fontFamily=myFont.value;
+}
+function changeCenter(){
+    if(document.getElementById('box').style.textAlign!="center")
+        document.getElementById('box').style.textAlign="center"; 
+    else  
+        document.getElementById('box').style.textAlign="left"; 
+}
+
+
 function changeWidth(){
     document.getElementById('box').style.width=myWidth.value; 
 }
